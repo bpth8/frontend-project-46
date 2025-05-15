31da0e96bb9024883df1905a55a727cb9cd9f858
bpth8 <itmy3denis@gmail.com>
2025-05-15 08:42:25 +0300

    реализована функция сравнения json - файлов без мутаций

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-function genDiff (filepath1, filepath2) {
-  return 'difference';
-}
+// eslint-disable-next-line import/no-extraneous-dependencies
+import _ from 'lodash';
+
+const genDiff = (obj1, obj2) => {
+  const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
+
+  const result = keys.map((key) => {
+    if (!Object.hasOwn(obj2, key)) {
+      return `  - ${key}: ${obj1[key]}`;
+    }
+    if (!Object.hasOwn(obj1, key)) {
+      return `  + ${key}: ${obj2[key]}`;
+    }
+    if (_.isEqual(obj1[key], obj2[key])) {
+      return `    ${key}: ${obj1[key]}`;
+    }
+    return [
+      `  - ${key}: ${obj1[key]}`,
+      `  + ${key}: ${obj2[key]}`,
+    ].join('\n');
+  });
+
+  return `{\n${result.join('\n')}\n}`;
+};
 
 export default genDiff;
